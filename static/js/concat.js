5dc1b77a4efe7e510e8a16021152835fcccc68c8
Lee Neenan <lneenan@pagemasters.com.au>
2017-08-14 12:15:05 +1000

    changed size of profile image from cloudinary

--- static/js/concat.js
+++ static/js/concat.js
@@ -29443,7 +29443,7 @@ HomeController.Listing = (function ($) {
                             
                                                         
                             var ImageUrl = $.image({media:data.articles[i]['featuredMedia'], mediaOptions:{width: 500 ,height:350, crop: 'limit'} });
-                            var profileImage = $.image({media:data.articles[i]['createdBy']['media'], mediaOptions:{width: 50 ,height:50, crop: 'limit'} });
+                            var profileImage = $.image({media:data.articles[i]['createdBy']['media'], mediaOptions:{width: 34 ,height:34, crop: 'limit'} });
                             data.articles[i]['imageUrl'] = ImageUrl;
                             data.articles[i]['profileImg'] = profileImage;
                             var articleId = parseInt(data.articles[i].articleId);
