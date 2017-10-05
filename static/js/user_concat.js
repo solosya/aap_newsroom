24c6aae3893dc36c1b4e6a32e1115a4f89a19150
Rohan Bassett <rbassett@pagemasters.com.au>
2017-10-05 11:59:58 +1100

    gulp

--- static/js/user_concat.js
+++ static/js/user_concat.js
@@ -21310,7 +21310,7 @@ HomeController.Listing = (function ($) {
                             
                                                         
                             var ImageUrl = $.image({media:data.articles[i]['featuredMedia'], mediaOptions:{width: 500 ,height:350, crop: 'limit'} });
-                            var profileImage = $.image({media:data.articles[i]['createdBy']['media'], mediaOptions:{width: 50 ,height:50, crop: 'limit'} });
+                            var profileImage = $.image({media:data.articles[i]['createdBy']['media'], mediaOptions:{width: 34 ,height:34, crop: 'thumb', gravity: 'face'} });
                             data.articles[i]['imageUrl'] = ImageUrl;
                             data.articles[i]['profileImg'] = profileImage;
                             var articleId = parseInt(data.articles[i].articleId);
@@ -21554,7 +21554,7 @@ $('document').ready(function() {
     var cardHolder = '';
     clearTimeout(cardHolder);
     cardHolder = setTimeout((function() {
-        $('.card .content > p, .card h2').dotdotdot({
+        $('.card .content > p, .card h2, .card .content > .author > p').dotdotdot({
             watch: true
         });
     }), 750);
