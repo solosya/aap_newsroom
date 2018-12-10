bf0e56c8c6c7f403d69fffb363d44961ad2333ee
Lee <lneenan@pagemasters.com.au>
2018-12-10 14:39:25 +1100

    move inline js code to file

--- assets/scripts/dialog-ip.js
+++ assets/scripts/dialog-ip.js
@@ -2,6 +2,79 @@
   Dialog Confirmation Box
 ***                       ****/
 
+Acme.IPCheck = function() {
+
+    function dot2num(dot) {
+        var d = dot.split(".");
+        return ((((((
+               +d[0])   * 256 )
+             +(+d[1]))  * 256 )
+             +(+d[2]))  * 256 )
+             +(+d[3]
+        );
+    }
+
+    $(function() {
+        Acme.IPToken = new Acme.Token("IP_ACCOUNT");
+        var token = Acme.IPToken.getToken();
+
+        if (!token) {
+
+            $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
+            function(json) {
+
+                var IPAdresses = [
+                    "203.4.*.*", 
+                    "203.4.189.*", 
+                    "203.4.189.122", 
+                    "203.4.189.125-203.4.189.160", 
+                    "203.4.182.124-203.4.190.124"
+                ];
+
+                var userAccount = false;
+                var userIPInt = dot2num(json.ip);
+
+                for (var i = 0 ; i < IPAdresses.length ; i++) {
+                    var range = IPAdresses[i].split("-");
+                    
+                    // All IP addresses are converted to a range, however
+                    // we can start by just listing one.  The second ip
+                    // in the range will be added automtically
+                    if (range.length < 2) {
+
+                        // check for a wildcard character and replace with a zero
+                        // for the first item in the range, and 255 for the second item in the range
+                        if (range[0].indexOf("*") > -1) {
+                            var ip1 = range[0];
+                            range[0] = dot2num( ip1.replace(/\*/g, "0") );
+                            range.push( dot2num( ip1.replace(/\*/g, "255") ) );
+                        } else {
+                            range[0] = dot2num ( range[0] );
+                            range.push( range[0] );
+                        }
+
+                    // if both ip addresses are specified no need to check for wildcard
+                    } else {
+                        range[0] = dot2num ( range[0] );
+                        range[1] = dot2num ( range[1] );
+                    }
+
+                    if (userIPInt >= range[0] && userIPInt <= range[1]) {
+                        userAccount = true;
+                        break;
+                    }
+                }
+
+                if ( userAccount ) {
+                    Acme.IPNoticePopup = new Acme.IPNotice("modal", "signin-modal", {"main": "ipnotice"});
+                    Acme.IPNoticePopup.render("main", "Did you know your employer is a subscriber to Newsroom Pro?");
+                }
+            });
+        }
+    });
+}
+
+
 Acme.IPNotice = function(template, parent, layouts) {
 
     this.template = template;
