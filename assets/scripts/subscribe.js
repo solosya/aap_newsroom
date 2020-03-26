640d67d99005cee4e01c01ff12949e8255040a96
Puck Willis <PWillis@pagemasters.com.au>
2020-03-26 17:25:09 +1100

    updates

--- assets/scripts/subscribe.js
+++ assets/scripts/subscribe.js
@@ -1,19 +1,63 @@
 $('.j-mcsubscribe').click(function(event){
-    //mcSubscribe(event.currentTarget);
+    mcSubscribe(event.currentTarget);
     var email = $('#'+$(event.currentTarget).data('input')).val();
-    consile.log(email);
-    $('#mcpopup').data('email',email);
+    //console.log(email);
+    $('#j-box-'+$(event.currentTarget).data('type')).addClass('d-none');
+    $('#j-mcpopup').data('email',email);
+    $('#j-mcpopup').removeClass('d-none');
 });
 
-// $('.j-mcmultisubscribe').click(function(){
-//     var ticks = $
+$('.j-mcmultisubscribe').click(function(event){
+    var doSubscribe = false;
+    var subscribeData = {
+        "EMAIL":  $('#j-mcpopup').data('email'), 
+        "u": "e0ae259e8f9472b9c54037c25",
+        "id": "71de5c4b35"
+    };
 
-// });
+    if ($('#j-mccheckbox-daily').is(":checked")){
+        doSubscribe = true;
+        subscribeData["group[3][1]"] = 1;
+    };
+    if ($('#j-mccheckbox-weekly').is(":checked")){
+        doSubscribe = true;
+        subscribeData["group[3][2]"] = 2;
+    };
+    if ($('#j-mccheckbox-lockerroom').is(":checked")){
+        doSubscribe = true;
+        subscribeData["group[3][4]"] = 4;
+    };
+    if ($('#j-mccheckbox-readingroom').is(":checked")){
+        doSubscribe = true;
+        subscribeData["group[3][8]"] = 8;
+    };
+
+    //console.log(subscribeData);
+
+    // Acme.server.create("https://newsroom.us14.list-manage.com/subscribe/post", subscribeData)
+    // .then(function(r) {
+    //     console.log(r);
+    // });   
+
+});
+
+$('.j-mccancel').click(function(){
+    $('#j-mcpopup').data('email','');
+    $('#j-mcpopup').addClass('d-none');
+    $( "#j-mccheckbox-daily" ).prop( "checked", false );
+    $( "#j-box-daily" ).removeClass("d-none");
+    $( "#j-mccheckbox-weekly" ).prop( "checked", false );
+    $( "#j-box-weekly" ).removeClass("d-none");
+    $( "#j-mccheckbox-lockerroom" ).prop( "checked", false );
+    $( "#j-box-lockerroom" ).removeClass("d-none");
+    $( "#j-mccheckbox-readingroom" ).prop( "checked", false );
+    $( "#j-box-readingroom" ).removeClass("d-none");
+});
 
 
 mcSubscribe = function(object){
     self = $(object);
-    console.log(self.data('input'));
+    //console.log(self.data('input'));
     var subscribeData = {
         "EMAIL": $('#'+self.data('input')).val(), 
         "u": "e0ae259e8f9472b9c54037c25",
@@ -31,11 +75,11 @@ mcSubscribe = function(object){
     if (self.data("group[3][8]")) {
         subscribeData["group[3][8]"] = 8;
     }
-    console.log(subscribeData);
+    //console.log(subscribeData);
     
-    Acme.server.create("https://newsroom.us14.list-manage.com/subscribe/post", subscribeData)
-        .then(function(r) {
-            console.log(r);
-        });   
+    // Acme.server.create("https://newsroom.us14.list-manage.com/subscribe/post", subscribeData)
+    //     .then(function(r) {
+    //         console.log(r);
+    //     });   
 };                     
             
--- static/js/concat.js
+++ static/js/concat.js
@@ -37433,34 +37433,87 @@ if ($('#stripekey').length > 0) {
 } 
 $('.j-mcsubscribe').click(function(event){
     mcSubscribe(event.currentTarget);
-});     
+    var email = $('#'+$(event.currentTarget).data('input')).val();
+    console.log(email);
+    $('#j-box-'+$(event.currentTarget).data('type')).addClass('d-none');
+    $('#j-mcpopup').data('email',email);
+    $('#j-mcpopup').removeClass('d-none');
+});
+
+$('.j-mcmultisubscribe').click(function(event){
+    var doSubscribe = false;
+    var subscribeData = {
+        "EMAIL":  $('#j-mcpopup').data('email'), 
+        "u": "e0ae259e8f9472b9c54037c25",
+        "id": "71de5c4b35"
+    };
+
+    if ($('#j-mccheckbox-daily').is(":checked")){
+        doSubscribe = true;
+        subscribeData["group[3][1]"] = 1;
+    };
+    if ($('#j-mccheckbox-weekly').is(":checked")){
+        doSubscribe = true;
+        subscribeData["group[3][2]"] = 2;
+    };
+    if ($('#j-mccheckbox-lockerroom').is(":checked")){
+        doSubscribe = true;
+        subscribeData["group[3][4]"] = 4;
+    };
+    if ($('#j-mccheckbox-readingroom').is(":checked")){
+        doSubscribe = true;
+        subscribeData["group[3][8]"] = 8;
+    };
+
+    console.log(subscribeData);
+
+    // Acme.server.create("https://newsroom.us14.list-manage.com/subscribe/post", subscribeData)
+    // .then(function(r) {
+    //     console.log(r);
+    // });   
+
+});
+
+$('.j-mccancel').click(function(){
+    $('#j-mcpopup').data('email','');
+    $('#j-mcpopup').addClass('d-none');
+    $( "#j-mccheckbox-daily" ).prop( "checked", false );
+    $( "#j-box-daily" ).removeClass("d-none");
+    $( "#j-mccheckbox-weekly" ).prop( "checked", false );
+    $( "#j-box-weekly" ).removeClass("d-none");
+    $( "#j-mccheckbox-lockerroom" ).prop( "checked", false );
+    $( "#j-box-lockerroom" ).removeClass("d-none");
+    $( "#j-mccheckbox-readingroom" ).prop( "checked", false );
+    $( "#j-box-readingroom" ).removeClass("d-none");
+});
 
 
 mcSubscribe = function(object){
     self = $(object);
+    console.log(self.data('input'));
     var subscribeData = {
-        "EMAIL": $('#'+self.data['input']).val(), 
+        "EMAIL": $('#'+self.data('input')).val(), 
         "u": "e0ae259e8f9472b9c54037c25",
         "id": "71de5c4b35"
     };
-    if (self.data["group[3][1]"]) {
+    if (self.data("group[3][1]")) {
         subscribeData["group[3][1]"] = 1;
     }
-    if (self.data["group[3][2]"]) {
+    if (self.data("group[3][2]")) {
         subscribeData["group[3][2]"] = 2;
     }
-    if (self.data["group[3][4]"]) {
+    if (self.data("group[3][4]")) {
         subscribeData["group[3][4]"] = 4;
     }
-    if (self.data["group[3][8]"]) {
+    if (self.data("group[3][8]")) {
         subscribeData["group[3][8]"] = 8;
     }
     console.log(subscribeData);
-    return;
-    Acme.server.create("https://newsroom.us14.list-manage.com/subscribe/post", subscribeData)
-        .then(function(r) {
-            console.log(r);
-        });   
+    
+    // Acme.server.create("https://newsroom.us14.list-manage.com/subscribe/post", subscribeData)
+    //     .then(function(r) {
+    //         console.log(r);
+    //     });   
 };                     
             
 
