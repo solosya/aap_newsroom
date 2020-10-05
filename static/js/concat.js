89c1a47b31eca30f0033643e68c68ba1d1746571
Lee Neenan <lneenan@pagemasters.com.au>
2020-10-05 15:31:16 +1100

    parse js theme config from twig

--- static/js/concat.js
+++ static/js/concat.js
@@ -35742,64 +35742,64 @@ Acme.IPCheck = function() {
 
         if (!token) {
 
-            Acme.server.fetch(_appJsConfig.appHostName + '/api/theme/get-config').done(function(r) {
+            // Acme.server.fetch(_appJsConfig.appHostName + '/api/theme/get-config').done(function(r) {
 
-                if (r.success === 1) {
+            //     if (r.success === 1) {
 
-                    if (typeof r.data.IPAdresses == 'undefined') {
-                        return;
-                    }
-                    IPAdresses = r.data.IPAdresses;
+                if (typeof window.IPAdresses == 'undefined') {
+                    return;
+                }
+                IPAdresses = window.IPAdresses;
+                $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
+                function(json) {
+                    console.log(json);
+                    var userAccount = false;
+                    var userIPInt = dot2num(json.ip);
 
-                    $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
-                    function(json) {
-                        var userAccount = false;
-                        var userIPInt = dot2num(json.ip);
-
-                        for (var i = 0 ; i < IPAdresses.length ; i++) {
-                            if (IPAdresses[i].indexOf('//') === 0 ) {
-                                continue;
-                            }
-        
-                            var range = IPAdresses[i].split("-");
-                            
-                            // All IP addresses are converted to a range, however
-                            // we can start by just listing one.  The second ip
-                            // in the range will be added automtically
-                            if (range.length < 2) {
-        
-                                // check for a wildcard character and replace with a zero
-                                // for the first item in the range, and 255 for the second item in the range
-                                if (range[0].indexOf("*") > -1) {
-                                    var ip1 = range[0];
-                                    range[0] = dot2num( ip1.replace(/\*/g, "0") );
-                                    range.push( dot2num( ip1.replace(/\*/g, "255") ) );
-                                } else {
-                                    range[0] = dot2num ( range[0] );
-                                    range.push( range[0] );
-                                }
-        
-                            // if both ip addresses are specified no need to check for wildcard
+                    for (var i = 0 ; i < IPAdresses.length ; i++) {
+                        if (IPAdresses[i].indexOf('//') === 0 ) {
+                            continue;
+                        }
+    
+                        var range = IPAdresses[i].split("-");
+                        
+                        // All IP addresses are converted to a range, however
+                        // we can start by just listing one.  The second ip
+                        // in the range will be added automtically
+                        if (range.length < 2) {
+    
+                            // check for a wildcard character and replace with a zero
+                            // for the first item in the range, and 255 for the second item in the range
+                            if (range[0].indexOf("*") > -1) {
+                                var ip1 = range[0];
+                                range[0] = dot2num( ip1.replace(/\*/g, "0") );
+                                range.push( dot2num( ip1.replace(/\*/g, "255") ) );
                             } else {
                                 range[0] = dot2num ( range[0] );
-                                range[1] = dot2num ( range[1] );
+                                range.push( range[0] );
                             }
-        
-                            if (userIPInt >= range[0] && userIPInt <= range[1]) {
-                                userAccount = true;
-                                break;
-                            }
+    
+                        // if both ip addresses are specified no need to check for wildcard
+                        } else {
+                            range[0] = dot2num ( range[0] );
+                            range[1] = dot2num ( range[1] );
                         }
-        
-                        if ( userAccount ) {
-                            Acme.IPNoticePopup = new Acme.IPNotice("modal", "ipdialog", {"main": "ipnotice"});
-                            Acme.IPNoticePopup.render("main", "Did you know your employer is a subscriber to Newsroom Pro?");
+    
+                        if (userIPInt >= range[0] && userIPInt <= range[1]) {
+                            userAccount = true;
+                            break;
                         }
-                    });
+                    }
+    
+                    if ( userAccount ) {
+                        Acme.IPNoticePopup = new Acme.IPNotice("modal", "ipdialog", {"main": "ipnotice"});
+                        Acme.IPNoticePopup.render("main", "Did you know your employer is a subscriber to Newsroom Pro?");
+                    }
+                });
 
 
-                } 
-            }).fail(function(r) { console.log(r);return;});
+            //     } 
+            // }).fail(function(r) { console.log(r);return;});
 
 
 
