79f32e24645a7e7b6af96378e2b6e43fe00ca61c
Lee <Leeneenan@gmail.com>
2021-03-25 10:05:43 +1100

    js gulp

--- static/js/concat.js
+++ static/js/concat.js
@@ -16245,31 +16245,37 @@ $('.j-mcprosubscribe').on('click',function(event){
 
 
 var footerCta = document.getElementById('cta-footer-button');
-footerCta.addEventListener('click', function(e) {
-    var form = document.querySelector('.j-cta-footer-form');
-    this.classList.add('d-none');
-    form.classList.remove('d-none');
-});
+if (footerCta) {
+    footerCta.addEventListener('click', function(e) {
+        var form = document.querySelector('.j-cta-footer-form');
+        this.classList.add('d-none');
+        form.classList.remove('d-none');
+    });
+}
 
 var bodyCta = document.getElementById('cta-body-signtoggle');
-bodyCta.addEventListener('click', function(e) {
-    this.classList.add('d-none');
-    var buttons = document.querySelectorAll('.j-cta-body-buttons');
-    for (var i=0; i<buttons.length; i++) {
-        buttons[i].classList.add('d-none');
-        buttons[i].classList.remove('d-md-block');
-    }
-    var form = document.querySelector('.j-cta-body-form');
-    form.classList.remove('d-none');
-});
+if (bodyCta) {
+    bodyCta.addEventListener('click', function(e) {
+        this.classList.add('d-none');
+        var buttons = document.querySelectorAll('.j-cta-body-buttons');
+        for (var i=0; i<buttons.length; i++) {
+            buttons[i].classList.add('d-none');
+            buttons[i].classList.remove('d-md-block');
+        }
+        var form = document.querySelector('.j-cta-body-form');
+        form.classList.remove('d-none');
+    });
+}
 
 
 var sideCta = document.querySelector('.j-cta-side-signtoggle');
-sideCta.addEventListener('click', function(e) {
-    var form = document.querySelector('.j-cta-side-form');
-    this.classList.add('d-none');
-    form.classList.remove('d-none');
-});
+if (sideCta) {
+    sideCta.addEventListener('click', function(e) {
+        var form = document.querySelector('.j-cta-side-form');
+        this.classList.add('d-none');
+        form.classList.remove('d-none');
+    });
+}
 
 
 Acme.Token = function(tokenName) 
