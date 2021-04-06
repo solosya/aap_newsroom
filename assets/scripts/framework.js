c3a7c29dc6234ed83e52fc914986108c907e5fdd
Lee <Leeneenan@gmail.com>
2021-04-06 15:49:55 +1000

    fix ajax setup

--- assets/scripts/framework.js
+++ assets/scripts/framework.js
@@ -30,18 +30,18 @@
             queryParams = (typeof queryParams !== 'undefined') ? queryParams : {};
             
             var url = (uri.indexOf("http") === 0) ? uri : _appJsConfig.appHostName + uri;
-            var token = $('meta[name="csrf-token"]').attr("content");
-            $.ajaxSetup({
-                beforeSend: function(xhr) {
-                    xhr.setRequestHeader('Csrf-Token', token);
-                }
-            });
-        
+            
             return $.ajax({
                 url: url,
                 data: queryParams,
                 dataType: datatype || "json",
-                type: type
+                type: type,
+                beforeSend: function(xhr) {
+                    if (type !== 'get' && url.indexOf('https://hivenews') === -1) {
+                        var token = $('meta[name="csrf-token"]').attr("content");
+                        xhr.setRequestHeader('x-csrf-token', token);
+                    }
+                }
             }).fail(function(r) {
                 // console.log(r);
                 if (r.status == 501 || r.status == 404) console.log(r.responseText);
