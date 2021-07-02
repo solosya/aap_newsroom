146a3c5f251f063bcd811c1c976b0357958e6322
insphere_mayank <insphere.mayank@gmail.com>
2021-07-02 14:36:55 +0530

    Ticket 1 updates

--- assets/js_wp/scripts.js
+++ assets/js_wp/scripts.js
@@ -1,32 +1,47 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    
-    // Lazyload, dotdotdot and owlCarousel curently run externallly to webpack bundle
-    
-    $("img.lazyload").lazyload({
-        effect : "fadeIn"
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Lazyload, dotdotdot and owlCarousel curently run externallly to webpack bundle
+
+  $("img.lazyload").lazyload({
+    effect: "fadeIn",
+  });
+
+  var cardHolder = "";
+  clearTimeout(cardHolder);
+  cardHolder = setTimeout(function () {
+    $(".j-truncate").dotdotdot({
+      watch: true,
     });
+  }, 750);
 
+  $("#owl-gallery-image").owlCarousel({
+    items: 1,
+    thumbs: true,
+    thumbsPrerendered: true,
+    URLhashListener: true,
+    startPosition: "URLHash",
+    pagination: true,
+    dots: false,
+    nav: true,
+    navText: ["", ""],
+  });
 
-    var cardHolder = '';
-    clearTimeout(cardHolder);
-    cardHolder = setTimeout((function() {
-        $('.j-truncate').dotdotdot({
-            watch: true
-        });
-    }), 750);
+  setTimeout(function () {
+    $(".article_content figure img").each(function () {
+      var width = $(this).width() + "px";
+      var captionObj = $(this).closest("figure").find("figcaption");
+      if (captionObj) {
+        captionObj.css({ width: width, display: "block" });
+      }
+    });
+  }, 400);
 
-    $("#owl-gallery-image").owlCarousel({
-        items: 1,
-        thumbs: true,
-        thumbsPrerendered: true,
-        URLhashListener:true,
-        startPosition: 'URLHash',
-        pagination: true,
-        dots: false,
-        nav: true,
-        navText: [
-            "",
-            ""
-        ]
-    });   
+  $(window).resize(function () {
+    $(".article_content figure img").each(function () {
+      var width = $(this).width() + "px";
+      var captionObj = $(this).closest("figure").find("figcaption");
+      if (captionObj) {
+        captionObj.css({ width: width, display: "block" });
+      }
+    });
+  });
 });
